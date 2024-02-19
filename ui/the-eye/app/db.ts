e47b0e5c1a01c3b50235b296c9a9c6fc39a9cf62
bradenmosley <144081350+bradenmosley@users.noreply.github.com>
2024-02-19 09:00:53 -0600

    Updated PrismaClient

---
+++ ui/the-eye/app/db.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from '@prisma/client'
+
+// Creates a single connection to the database to fix the Next.js reload issue
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined
+}
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["query"],
+  })
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+// ---
+
+async function main() {
+  // ... you will write your Prisma Client queries here
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
