e727cc85fb4bfc1de9e88f74eb3e38ef0bd5e912
bradenmosley <144081350+bradenmosley@users.noreply.github.com>
2024-02-20 17:08:47 -0600

    Commented out prisma

--- ui/the-eye/app/the-eye/player/[playerId]/page.tsx
+++ ui/the-eye/app/the-eye/player/[playerId]/page.tsx
@@ -1,21 +1,21 @@
 import * as React from 'react';
 import Box from "@mui/material/Box";
-import { prisma } from '@/app/db';
+// import { prisma } from '@/app/db';
 
 export default async function PlayerPage({ params }: { params: { playerId: string } }) {
-    const player = await prisma.player.findUnique({
-        where: {
-            id: params.playerId,
-        }
-    });
+    // const player = await prisma.player.findUnique({
+    //     where: {
+    //         id: params.playerId,
+    //     }
+    // });
     
     return (
 
         <Box>
-            <h3>Player ID: {params.playerId}</h3>
+            {/* <h3>Player ID: {params.playerId}</h3>
             <h3>Player Name: {player?.name}</h3>
             <h3># At Bats: {player?.at_bats}</h3>
-            <h3>K%: {player?.k_percentage}</h3>
+            <h3>K%: {player?.k_percentage}</h3> */}
         </Box>
         
     );
