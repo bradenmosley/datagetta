8821996bc0059000dc784be66742efb80ce5b496
Caden Garrett <garrettcs11@gmail.com>
2024-03-12 21:44:27 -0500

    Updated sql

--- database/sql/drop-all.sql
+++ database/sql/drop-all.sql
@@ -1,17 +1,17 @@
-DROP TABLE public.conferences CASCADE;
+DROP TABLE IF EXISTS public.conferences CASCADE;
 
-DROP TABLE public.pitch_sums CASCADE;
+DROP TABLE IF EXISTS public.pitch_sums CASCADE;
 
-DROP TABLE public.players CASCADE;
+DROP TABLE IF EXISTS public.players CASCADE;
 
-DROP TABLE public.seasons CASCADE;
+DROP TABLE IF EXISTS public.seasons CASCADE;
 
-DROP TABLE public.teams CASCADE;
+DROP TABLE IF EXISTS public.teams CASCADE;
 
-DROP TABLE public.trackman_batter CASCADE;
+DROP TABLE IF EXISTS public.trackman_batter CASCADE;
 
-DROP TABLE public.trackman_catcher CASCADE;
+DROP TABLE IF EXISTS public.trackman_catcher CASCADE;
 
-DROP TABLE public.trackman_metadata CASCADE;
+DROP TABLE IF EXISTS public.trackman_metadata CASCADE;
 
-DROP TABLE public.trackman_pitcher CASCADE;
+DROP TABLE IF EXISTS public.trackman_pitcher CASCADE;
