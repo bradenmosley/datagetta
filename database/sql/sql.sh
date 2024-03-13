af163f5bc6364196d702bdf5237173cf9069aed3
Caden Garrett <garrettcs11@gmail.com>
2024-03-13 15:00:47 -0500

    Changed sql.sh to more simple text

--- database/sql/sql.sh
+++ database/sql/sql.sh
@@ -1,4 +1,1 @@
-psql -U $POSTGRES_USER -d $POSTGRES_DB < drop-all.sql
-psql -U $POSTGRES_USER -d $POSTGRES_DB < pre-schema.sql
-psql -U $POSTGRES_USER -d $POSTGRES_DB < schema_seniordesign_1.sql
-psql -U $POSTGRES_USER -d $POSTGRES_DB < views.sql
+psql -U $POSTGRES_USER -d $POSTGRES_DB
