6d50ab98f30b2c6082285e4f70f283e505d5fb9b
bradenmosley <144081350+bradenmosley@users.noreply.github.com>
2024-03-19 15:59:51 -0500

    Update bigInt.ts

--- ui/the-eye/app/bigInt.ts
+++ ui/the-eye/app/bigInt.ts
@@ -1,9 +1,2 @@
-// https://github.com/GoogleChromeLabs/jsbi/issues/30#issuecomment-1466898661
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore: Unreachable code error
-// BigInt.prototype.toJSON = function (): String {
-//     return this.toString();
-// };
-
 const replacer = (key: any, value: any) =>
     typeof value === 'bigint' ? { $bigint: value.toString() } : value;
