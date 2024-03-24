309dff24efacd5683fe11a00f1cfa14c640dd54e
bradenmosley <144081350+bradenmosley@users.noreply.github.com>
2024-03-23 21:54:37 -0500

    Added Team DisplayName SQL

---
+++ database/sql/AssignTeam_DisplayName_Conference.sql
@@ -0,0 +1,90 @@
+-- Atlantic Coast Conference
+update "teams" set "DisplayName" = 'Louisville', "Conference" = 'Atlantic Coast Conference' where "TeamName" = 'LOU_CAR';
+
+-- Big 12 Conference
+update "teams" set "DisplayName" = 'TCU', "Conference" = 'Big 12 Conference' where "TeamName" = 'TCU_HFG';
+
+-- Big Ten Conference
+update "teams" set "DisplayName" = 'Rutgers', "Conference" = 'Big Ten Conference' where "TeamName" = 'RUT_SCA';
+
+-- PAC-12 Conference
+update "teams" set "DisplayName" = 'Stanford', "Conference" = 'PAC-12 Conference' where "TeamName" = 'STA_CAR';
+
+-- Southeastern Conference
+update "teams" set "DisplayName" = 'Tennessee', "Conference" = 'Southeastern Conference' where "TeamName" = 'TEN_VOL';
+update "teams" set "DisplayName" = 'Florida', "Conference" = 'Southeastern Conference' where "TeamName" = 'FLA_GAT';
+
+-- American Athletic Conference
+update "teams" set "DisplayName" = 'UCF', "Conference" = 'American Athletic Conference' where "TeamName" = 'UCF_KNI';
+
+-- Big South Conference
+update "teams" set "DisplayName" = 'Campbell', "Conference" = 'Big South Conference' where "TeamName" = 'CAM_CAM';
+
+-- Coastal Athletic Association
+update "teams" set "DisplayName" = 'Col. of Charleston', "Conference" = 'Coastal Athletic Association' where "TeamName" = 'COL_CHA';
+
+-- Conference USA
+update "teams" set "DisplayName" = 'Louisiana Tech', "Conference" = 'Conference USA' where "TeamName" = 'LOU_BUL';
+
+-- American East Conference
+update "teams" set "DisplayName" = 'UMass Lowell', "Conference" = 'American East Conference' where "TeamName" = 'UMASS_RIV';
+
+-- Atlantic Sun Conference
+update "teams" set "DisplayName" = 'Liberty', "Conference" = 'Atlantic Sun Conference' where "TeamName" = 'LIB_FLA';
+
+-- Atlantic 10 Conference
+update "teams" set "DisplayName" = 'Davidson', "Conference" = 'Atlantic 10 Conference' where "TeamName" = 'DAV_WIL';
+
+-- Big East Conference
+update "teams" set "DisplayName" = 'Creighton', "Conference" = 'Big East Conference' where "TeamName" = 'CRE_BLU';
+
+-- Big West Conference
+update "teams" set "DisplayName" = 'UC Santa Barbara', "Conference" = 'Big West Conference' where "TeamName" = 'SAN_GAU';
+
+-- Horizon League
+update "teams" set "DisplayName" = 'Wright St.', "Conference" = 'Horizon League' where "TeamName" = 'WRI_RAI';
+
+-- The Ivy League
+update "teams" set "DisplayName" = 'Penn', "Conference" = 'The Ivy League' where "TeamName" = 'PEN_QUA';
+
+-- Metro Atlantic Athletic Conference
+update "teams" set "DisplayName" = 'Fairfield', "Conference" = 'Metro Atlantic Athletic Conference' where "TeamName" = 'FAI_STA';
+
+-- Mid-American Conference
+update "teams" set "DisplayName" = 'Ball St.', "Conference" = 'Mid-American Conference' where "TeamName" = 'BAL_CAR';
+
+-- Missouri Valley Conference
+update "teams" set "DisplayName" = 'Southern Ill.', "Conference" = 'Missouri Valley Conference' where "TeamName" = 'SIU_SAL';
+
+-- Mountain West Conference
+update "teams" set "DisplayName" = 'UNLV', "Conference" = 'Mountain West Conference' where "TeamName" = 'UNL_REB';
+
+-- Northeast Conference
+update "teams" set "DisplayName" = 'LIU', "Conference" = 'Northeast Conference' where "TeamName" = 'LON_ISL22';
+
+-- Ohio Valley Conference
+update "teams" set "DisplayName" = 'Southeast Mo. St.', "Conference" = 'Ohio Valley Conference' where "TeamName" = 'SOU_RED';
+
+-- Patriot League
+update "teams" set "DisplayName" = 'Army West Point', "Conference" = 'Patriot League' where "TeamName" = 'ARM_BLA';
+
+-- Southern Conference
+update "teams" set "DisplayName" = 'Wofford', "Conference" = 'Southern Conference' where "TeamName" = 'WOF_TER';
+
+-- Southland Conference
+update "teams" set "DisplayName" = 'McNeese', "Conference" = 'Southland Conference' where "TeamName" = 'MCN_COW';
+
+-- Sun Belt Conference
+update "teams" set "DisplayName" = 'Texas St.', "Conference" = 'Sun Belt Conference' where "TeamName" = 'TEX_BOB';
+
+-- Southwestern Athletic Conference
+update "teams" set "DisplayName" = 'Alabama St.', "Conference" = 'Southwestern Athletic Conference' where "TeamName" = 'ALA_HOR';
+
+-- The Summit League
+update "teams" set "DisplayName" = 'North Dakota St.', "Conference" = 'The Summit League' where "TeamName" = 'NOR_BIS';
+
+-- West Coast Conference
+update "teams" set "DisplayName" = 'Gonzaga', "Conference" = 'West Coast Conference' where "TeamName" = 'GON_BUL';
+
+-- Western Athletic Conference
+update "teams" set "DisplayName" = 'Sam Houston State', "Conference" = 'Western Athletic Conference' where "TeamName" = 'SAM_BEA';
