4760db56636cb103cefd9f908550fcecbc088a8c
Caden Garrett <garrettcs11@gmail.com>
2024-03-25 12:46:41 -0500

    A

--- database/sql/keepd1.sql
+++ database/sql/keepd1.sql
@@ -1,1 +1,1 @@
-delete from teams t2 where t2."TeamName" in (select t."TeamName" from teams t where t."Conference" = 'NotSet' limit 5);
+delete from teams t2 where t2."TeamName" in (select t."TeamName" from teams t where t."Conference" = 'NotSet' limit 1);
