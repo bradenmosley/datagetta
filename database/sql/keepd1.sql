f1a920d5aba9a9905b4d98e09f7a600e4c8ca0bd
Caden Garrett <garrettcs11@gmail.com>
2024-03-25 13:13:14 -0500

    Updated sql

--- database/sql/keepd1.sql
+++ database/sql/keepd1.sql
@@ -1,1 +1,4 @@
-delete from teams t2 where t2."TeamName" in (select t."TeamName" from teams t where t."Conference" = 'NotSet' limit 3);
+delete from teams t2 where t2."TeamName" in (select t."TeamName" from teams t where t."Conference" = 'NotSet');
+
+-- delete from trackman_metadata tm where tm."AwayTeam" not in (select t."TeamName" from teams t);
+-- delete from trackman_metadata tm where tm."HomeTeam" not in (select t."TeamName" from teams t);
